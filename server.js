5275fbcc6dd649af16e80c8b58f26d97a618d462
Donald McKendrick <ddmckendrick@gmail.com>
2015-02-19 15:00:39 +0000

    remove outdated code

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var app = express();
 
 app.use(prerender.set('prerenderToken', 'NBDy0Xc1435JXDrCppLQ')); 
 app.use(express.static("public"));
-app.use(app.router); 
 
 // This will ensure that all routing is handed over to AngularJS 
 app.get('*', function(req, res){ 

cc951cfac2106cbfa7c2a122918585bf0826e407
Donald McKendrick <ddmckendrick@gmail.com>
2015-02-20 15:45:16 +0000

    add handlebars and switch to remote assets

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var prerender = require('prerender-node');
+var exphbs = require('express-handlebars');
 var app = express();
+var hbs = exphbs.create({
+  helpers: {
+    assetUrl: function () { 
+      return process.env.FMF_ASSET_PATH;
+    } 
+  }
+});
 
-
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(prerender.set('prerenderToken', 'NBDy0Xc1435JXDrCppLQ')); 
-app.use(express.static("public"));
 
 // This will ensure that all routing is handed over to AngularJS 
 app.get('*', function(req, res){ 
-  res.sendfile('./public/index.html'); 
+  res.render('index', {layout: false});
 });
 
 app.listen(process.env.PORT || 8081); 
 console.log("Go Prerender Go!");
+console.log(process.env.FMF_ASSET_PATH);

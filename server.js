c41eca9992e3ac899223e6a8113e995bfa5d076e
Donald McKendrick <ddmckendrick@gmail.com>
2015-02-21 15:12:58 +0000

    remove req https code

--- server.js
+++ server.js
@@ -10,20 +10,9 @@ var hbs = exphbs.create({
   }
 });
 
-function requireHTTPS(req, res, next) {
-  if (!req.secure) {
-      //FYI this should work for local development as well
-      return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
-
-
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.use(prerender.set('prerenderToken', 'NBDy0Xc1435JXDrCppLQ')); 
-app.use(requireHTTPS);
+app.use(prerender.set('prerenderToken', 'NBDy0Xc1435JXDrCppLQ'));
 // This will ensure that all routing is handed over to AngularJS 
 app.get('*', function(req, res){ 
   res.render('index', {layout: false});

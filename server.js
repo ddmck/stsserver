f42494e7468f4e9eb0459ae72a316c4fabc39202
Donald McKendrick <ddmckendrick@gmail.com>
2015-02-21 19:07:24 +0000

    try again with ssl-ifying things

--- server.js
+++ server.js
@@ -9,10 +9,22 @@ var hbs = exphbs.create({
     } 
   }
 });
+var env = process.env.NODE_ENV || 'development';
+
+var forceSsl = function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(prerender.set('prerenderToken', 'NBDy0Xc1435JXDrCppLQ'));
+if (env === 'production') {
+  app.use(forceSsl);
+}
 // This will ensure that all routing is handed over to AngularJS 
 app.get('*', function(req, res){ 
   res.render('index', {layout: false});
